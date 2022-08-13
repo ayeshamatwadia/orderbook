594f37ffd5974c2d9433c4046a84bb684fb2ca38
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2022-08-13 12:17:21 +0200

    starter rest project

---
+++ src/test/java/com/test/orderbook/OrderbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.orderbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
